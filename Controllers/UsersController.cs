f193c22a07b78a735fea41bd9e879f89ac037bf5
Eduardo Russo <Eduardo.Russo@jato.com>
2020-05-05 08:42:25 +0100

    first commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using dotnet.jwt.Services;
+using dotnet.jwt.Models;
+using System;
+
+namespace dotnet.jwt.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody]AuthenticateModel model)
+        {
+            var user = _userService.Authenticate(model.Username, model.Password);
+
+            if (user == null) {
+                return BadRequest(new { message = "Username or password is incorrect" });
+            }
+            return Ok(user);
+        }
+    }
+}
---
+++ Models/GlossaryItem.cs
@@ -0,0 +1,8 @@
+namespace dotnet.jwt.Models
+{
+    public class GlossaryItem
+    {
+        public string Term { get; set; }
+        public string Definition { get; set; }
+    }
+}
